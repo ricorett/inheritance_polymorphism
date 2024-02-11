38974e45ec9416eb340c8023f48526d85afc4bc9
Lana <115147586+ricorett@users.noreply.github.com>
2024-02-11 20:38:36 +0300

    didnt test yet

--- 02/eq_triangle.h
+++ 02/eq_triangle.h
@@ -2,22 +2,13 @@
 #include "triangle.h"
 
 
-// class Equilateral_triangle : public Triangle{
-// public:
-//     Equilateral_triangle(double a, double A) : Triangle(a,  a, a, A, A, A ){
-//         name = std::string("Равносторонний треугольник");
-//     }
-    
-// };
-class Equilateral_triangle : public Triangle
+class Equilateral_triangle : public Triangle //eq
 {
 public:
-    /*
-    * Чтобы заюзать равносторонний треугольник, надо его создать. Где мы создаем объект?
-    * В конструкторе. Одна сторона, один угол. 
-    */
+
     Equilateral_triangle(double a, double A) : Triangle(a, a, a, A, A, A)
     {
         name = std::string("Равносторонний треугольник");
     }
 };
+
--- 02/main.cpp
+++ 02/main.cpp
@@ -11,21 +11,17 @@
 void print_info(Figure *f);
 
 int main()
-
 {
-   // setlocale(LC_ALL, "Russian"); // Я запускаю MS VC++ под винду. мне это надо. тебе это не надо.
-    // Поехали, создаем фигуры
     Figure f;
     Triangle t(10, 20, 30, 30, 40, 110);
-    // Кстати, угол 110 градусов - тупой. Такой же тупой как тот, кто писал условия задачи.
-    Equilateral_triangle rb(10, 20, 45, 90);
-    Right_Triangle r(10, 20);
-    Isosceles_triangle prt(10, 20, 30, 40, 50);
+    Isosceles_triangle rb(10, 20, 45, 90);
+    Equilateral_triangle r(10, 20);
+    Right_Triangle prt(10, 20, 30, 40, 50);
     Parallelogramm par(10, 20, 30, 40, 50, 60, 70, 80);
     Rectangle pry(10, 20);
     Square kva(10);
     Rhombus rom(10, 20, 30);
-    // Вызываем для каждой фигуры print_info
+
     print_info(&f);
     print_info(&t);
     print_info(&rb);
@@ -43,7 +39,7 @@ void print_info(Figure* f)
 {
     std::cout << f->get_figure_name() << ":" << std::endl;
    std::cout << "Стороны: ";
-    for (int i = 0; i < f->get_side_count(); i++) std::cout << (char)('a' + i) << "=" << f->get_side(i) << " ";     // Если к char прибавить число, то будет следующий char. 'a' + 1 = 'b'. Такие дела.
+    for (int i = 0; i < f->get_side_count(); i++) std::cout << (char)('a' + i) << "=" << f->get_side(i) << " ";    
     std::cout <<std::endl << "Углы: ";                                                                                   
     for (int i = 0; i < f->get_side_count(); i++) std::cout << (char)('A' + i) << "=" << f->get_angle(i) << " ";
    std::cout << std::endl << std::endl;
